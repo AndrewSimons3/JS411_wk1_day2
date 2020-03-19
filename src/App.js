c2fd4e9b903bbdfcbbf671c8bf3180b87c67a559
AndrewSimons3 <simdrew07@gmail.com>
2020-03-18 20:25:31 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -13,26 +13,45 @@ class App extends React.Component {
     beers: []
   }
   componentDidMount() {
-    fetch("/https://api.punkapi.com/v2/beers").then( (resp) => {
+    fetch("https://api.punkapi.com/v2/beers").then( (resp) => {
       return resp.json();
   }).then( (data) => {
-    this.setState()
+    this.setState({beers: data})
   });
   }
 
+  likeBeer(beer) {
+    let newBeers = this.state.beers.map((aBeer) => {
+      //new array to set the new state of beers so one of beers has a different value for isLiked
+      if(aBeer.id === beer.id) {
+        aBeer.isLiked = !aBeer.isLiked
+      }
+      return aBeer
+    })
+
+    this.setState({beers: newBeers})
+    
+    console.log(beer)
+  }
+
   render() {
+    
     return (
       <div>
         <h1>
           Beer List 
         </h1>
-        {this.state.beers.map((beer) => {
-          console.log(beer)
-          return <h2>{beer.name}</h2>
+        <ul>
+        {this.state.beers.map((beer, index) => {
+          return(<li key={index}>
+            <span>{beer.name}</span>
+            <button onClick={this.likeBeer.bind(this, beer)}>{beer.isLiked ? "Unlike" : "Like"}</button>
+          </li>)
         })}
+        </ul>
       </div>
     );
   }
-  }
+}
 
 export default App;
